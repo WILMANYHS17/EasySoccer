1d51765ba24b79468559aa9c35986324616fb638
Wilman <whernandezsuesca@gmail.com>
2022-10-01 18:49:40 -0500

    create view pager

---
+++ app/src/main/java/com/wilman/easysoccer/models/Picture.kt
@@ -0,0 +1,3 @@
+package com.wilman.easysoccer.models
+
+data class Picture(val id:String, val url: String, val secure_url: String)
--- app/src/main/java/com/wilman/easysoccer/ui/homeAdmin/HomeAdminFragment.kt
+++ app/src/main/java/com/wilman/easysoccer/ui/homeAdmin/HomeAdminFragment.kt
@@ -6,12 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.wilman.easysoccer.databinding.FragmentHomeAdminBinding
 import com.wilman.easysoccer.ui.header.HeaderViewProfile
+import com.wilman.easysoccer.ui.viewpager.ImagesDetailAdapter
+import kotlin.math.abs
 
 class HomeAdminFragment : Fragment() {
 
     private var _binding: FragmentHomeAdminBinding? = null
+
+    private val viewPagerAdapter by lazy {
+        ImagesDetailAdapter(
+            ::showImageZoom
+        )
+    }
 
     private val binding get() = _binding!!
 
@@ -26,6 +37,8 @@ class HomeAdminFragment : Fragment() {
         _binding = FragmentHomeAdminBinding.inflate(inflater, container, false)
         val root: View = binding.root
         HeaderViewProfile(_binding!!.headerProfile, this).build()
+        viewPagerAdapter.setListImage(arrayListOf())
+        setUpAdapter()
 
         homeAdminViewModel.text.observe(viewLifecycleOwner) {
         }
@@ -36,4 +49,26 @@ class HomeAdminFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setUpAdapter() {
+        binding.viewpager.apply {
+            clipToPadding = false
+            clipChildren = false
+            offscreenPageLimit = 3
+            adapter = viewPagerAdapter
+
+            val compositePageTransformer = CompositePageTransformer()
+            val pageTransformer = ViewPager2.PageTransformer { page, position ->
+                var r = 1 - abs(position)
+                page.scaleY = 0.85f + r * 0.15f
+            }
+            with(compositePageTransformer) {
+                addTransformer(MarginPageTransformer(40))
+                addTransformer(pageTransformer)
+            }
+            setPageTransformer(compositePageTransformer)
+        }
+    }
+
+    private fun showImageZoom(position: Int) {}
 }
